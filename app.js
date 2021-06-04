430df27cb7b919577b0308e4fc2a1b09fc27a719
a-sauvaget <alexandre.sauaget44@gmail.com>
2021-06-04 12:49:04 +0200

    Add timeline to message flow

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+/* eslint-disable max-len */
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-await-in-loop */
+/* eslint-disable radix */
 /* eslint-disable no-undef */
 /* eslint-disable no-shadow */
 /* eslint-disable no-useless-return */
@@ -7,14 +11,24 @@ const session = require('express-session');
 // Session data needs to be stored somewhere
 const RedisStore = require('connect-redis')(session);
 const path = require('path');
+const { promisify } = require('util');
 const redis = require('redis');
 const bcrypt = require('bcrypt');
+const { formatDistance } = require('date-fns');
 
 // Acces to redis DB
 const client = redis.createClient({
   password: 'vm5eqlAZh0K4WaI8NY30/Rwm+SeB1M2wH/lrMbsgVCG8YGPnBuBJlGVgnyHXkj3i1hF8EiSrkKwwBAYO',
 });
 
+// Promisify functions - start with a for async
+const ahget = promisify(client.hget).bind(client);
+const asmembers = promisify(client.smembers).bind(client);
+const ahkeys = promisify(client.hkeys).bind(client);
+const aincr = promisify(client.incr).bind(client);
+const alrange = promisify(client.lrange).bind(client);
+
+// Create our Express
 const app = express();
 
 // Tells how to process data sent by form
@@ -40,9 +54,45 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // View to render for each path
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   if (req.session.userid) {
-    res.render('dashboard');
+    const currentUserName = await ahget(`user:${req.session.userid}`, 'username');
+    const following = await asmembers(`following:${currentUserName}`);
+    const users = await ahkeys('users');
+
+    const timeline = [];
+    const posts = await alrange(`timeline:${currentUserName}`, 0, 100);
+
+    for (post of posts) {
+      const timestamp = await ahget(`post:${post}`, 'timestamp');
+      const timeString = formatDistance(
+        new Date(),
+        new Date(parseInt(timestamp)),
+      );
+
+      timeline.push({
+        message: await ahget(`post:${post}`, 'message'),
+        author: await ahget(`post:${post}`, 'username'),
+        timeString,
+      });
+    }
+
+    res.render('dashboard', {
+      users: users.filter(
+        // Remove my user name from the user list
+        (user) => user !== currentUserName && following.indexOf(user) === -1,
+      ),
+      currentUserName,
+      timeline,
+    });
+  } else {
+    res.render('login');
+  }
+});
+
+app.get('/post', (req, res) => {
+  if (req.session.userid) {
+    res.render('post');
   } else {
     res.render('login');
   }
@@ -66,7 +116,7 @@ app.post('/', (req, res) => {
   const saveSessionAndRenderDashboard = (userid) => {
     req.session.userid = userid;
     req.session.save();
-    res.render('dashboard');
+    res.redirect('/');
   };
 
   const handleSignup = (username, password) => {
@@ -110,6 +160,56 @@ app.post('/', (req, res) => {
       handleLogin(userid, password);
     }
   });
+});
+
+// Post a message
+app.post('/post', async (req, res) => {
+  if (!req.session.userid) {
+    res.render('login');
+    return;
+  }
+
+  const { message } = req.body;
+  const currentUserName = await ahget(`user:${req.session.userid}`, 'username');
+  const postid = await aincr('postid');
+  client.hmset(
+    `post:${postid}`,
+    'userid',
+    req.session.userid,
+    'username',
+    currentUserName,
+    'message',
+    message,
+    'timestamp',
+    Date.now(),
+  );
+  client.lpush(`timeline:${currentUserName}`, postid);
+
+  const followers = await asmembers(`followers:${currentUserName}`);
+  for (follower of followers) {
+    client.lpush(`timeline:${follower}`, postid);
+  }
+
+  res.redirect('/');
+});
+
+// Track who we are following
+app.post('/follow', (req, res) => {
+  if (!req.session.userid) {
+    res.render('login');
+    return;
+  }
+
+  const { username } = req.body;
+
+  client.hget(`user:${req.session.userid}`, 'username', (err, currentUserName) => { // Adding 2 sets
+    client.sadd(`following:${currentUserName}`, username);
+    client.sadd(`followers:${username}`, currentUserName);
+  });
+  /* res.redirect is used to pass data to the template,
+  and if we are connected,
+  it show in every case the dashboard */
+  res.redirect('/');
 });
 
 // Active the server
